9dc3e39b3b894f1e4de79d2f8531bc8eeef91566
Виталий Еленчук <135379297+vitalii1408@users.noreply.github.com>
2026-07-08 05:38:45 +0300

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", () => {
+  const color = getRandomHexColor();
+
+  document.body.style.backgroundColor = color;
+  colorSpan.textContent = color;
+});
